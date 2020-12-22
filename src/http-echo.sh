daa8de6785d77a81c033a21972d8589c0f9006f5
Karsten Köth <karsten.koeth@gmail.com>
2020-12-22 12:55:15 +0100

    http REST API Server realized supporting GET

--- src/http-echo.sh
+++ src/http-echo.sh
@@ -6,9 +6,11 @@
 #
 # 2020-12-17 0.01 kdk First Version with version information
 # 2020-12-20 0.02 kdk With Port as parameter
+# 2020-12-21 0.03 kdk With CORS in http header
+# 2020-12-22 0.04 kdk Kill optimized to catch all
 
 PROG_NAME="HTTP Echo"
-PROG_VERSION="0.02"
+PROG_VERSION="0.04"
 PROG_SCRIPTNAME="http-echo.sh"
 
 # #########################################
@@ -56,31 +58,113 @@ PORT="10081"
 
 
 # #########################################
+# getSharingDirectory()
+# Parameter
+#   -
+# Return
+#   1 : Newly created sharing directory
+# Get directory for sharing.
+function getSharingDirectory()
+{
+    Uuid=$(uuidgen)
+    TmpDir="$HOME/tmp/http-echo_$Uuid"
+    mkdir -p -v "$TmpDir"
+    if [ ! -d "$TmpDir" ] ; then
+        echo "[$PROG_NAME:ERROR] Can't create temporary directoy. Exit."
+        exit
+    fi   
+    echo "$TmpDir"
+}
+
+
+# #########################################
+# serverText()
+# Parameter
+#   1 : Port number listen at
+# Return 
+#    -
+# This function starts the server for sending Content-Type: application/text
+function serverText()
+{
+    serverPort="$1"
+    serverProcess="http-text.sh"
+    BinaryDir=$(dirname "$0")
+    ServerScript="$BinaryDir/$serverProcess"
+    if [ ! -f "$ServerScript" ] ; then
+        echo "[$PROG_NAME:ERROR] Server process script not found. Exit."
+        exit
+    fi
+    #echo "[$PROG_NAME:serverText:DEBUG] '$0'  '$ServerScript'"    
+
+    SharingDir=$(getSharingDirectory)
+    #echo "[$PROG_NAME:serverText:DEBUG] '$SharingDir'"
+
+    socat -d -d TCP-L:$serverPort,fork EXEC:"$ServerScript $SharingDir"
+}
+
+
+# #########################################
+# serverSimple()
+# Parameter
+#   1 : Port number listen at
+# Return 
+#    -
+# This function starts a simple server not needing any additional files.
+function serverSimple()
+{
+    serverPort="$1"
+    # bash echo
+    #socat -T 1 -d -d TCP-L:10081,reuseaddr,fork,crlf SYSTEM:"echo -e \"\\\"HTTP/1.0 200 OK\\\nDocumentType: text/plain\\\n\\\ndate: \$\(date\)\\\nserver:\$SOCAT_SOCKADDR:\$SOCAT_SOCKPORT\\\nclient: \$SOCAT_PEERADDR:\$SOCAT_PEERPORT\\\n\\\"\"; cat; echo -e \"\\\"\\\n\\\"\""
+
+    # bsd echo, e.g. on MAC:
+    socat -T 1 -d -d TCP-L:$serverPort,reuseaddr,fork,crlf SYSTEM:"echo \"\\\"HTTP/1.0 200 OK\\\nDocumentType: text/plain\\\nAccess-Control-Allow-Origin: *\\\n\\\ndate: \$\(date\)\\\nserver: \$SOCAT_SOCKADDR:\$SOCAT_SOCKPORT\\\nclient: \$SOCAT_PEERADDR:\$SOCAT_PEERPORT\\\n\\\"\"; cat; echo \"\\\"\\\n\\\"\""
+}
+
+# #########################################
 # killProcesses()
+# Parameter
+#    1 : String (maybe with more lines) holding all PIDs
+# Return
+#    -
+# Kill all processes mentioned in the parameter
+function killProcesses()
+{
+    killProc=""
+    for killProc in $1
+    do
+        # Do NOT kill myself!
+        if [ $$ -ne $killProc ] ; then
+            # Only kill, if the process still runs (exclude grep, etc...)
+            runProc=$(ps -o pid=,command= $killProc)
+            if [ -n "$runProc" ] ; then
+                kill $killProc
+                echo "[$PROG_NAME:STATUS] '$killProc' killed."
+            fi
+        fi
+    done
+}
+
+# #########################################
+# killServer()
 # Parameter
 #    -
 # Return 
 #    -
 # Kill all processes regarding this program
-function killProcesses()
+function killServer()
 {
     # Kill all processes with this batch script name:
-    killMe=$(ps x | grep http-echo.sh | cut -f 1 -d " ")
-    for killProc in $killMe
-    do
-        # Do NOT kill myself!
-        if [ $$ -ne $killProc ] ; then
-            kill $killProc
-            echo "[$PROG_NAME:STATUS] '$killProc' killed."
-        fi
-    done
+    killMe=$(ps x -o pid,command | grep http-echo.sh | sed 's/^ \(.*\)/\1/' | cut -f 1 -d " ")
+    killProcesses "$killMe"
+
     # Kill all processes from socat with this program parameter inside the script:
-    killMe2=$(ps x | grep socat | grep DocumentType | cut -f 1 -d " ")
-    for killProc2 in $killMe2
-    do
-        kill $killProc2
-        echo "[$PROG_NAME:STATUS] '$killProc2' killed."
-    done
+    # serverSimple
+    killMe=$(ps x -o pid,command | grep socat | grep DocumentType | sed 's/^ \(.*\)/\1/' | cut -f 1 -d " ")
+    killProcesses "$killMe"
+
+    # serverText
+    killMe=$(ps x -o pid,command | grep socat | grep http-text.sh | sed 's/^ \(.*\)/\1/' | cut -f 1 -d " ")
+    killProcesses "$killMe"
 }
 
 # #########################################
@@ -139,7 +223,7 @@ if [ $# -ge 1 ] ; then
             fi
         fi
     elif [ "$1" = "-k" ] ; then
-        killProcesses ; exit;
+        killServer ; exit;
     elif [ "$1" = "-V" ] ; then
         echo "[$PROG_NAME:STATUS] Version $PROG_VERSION" ; exit;
     elif [ "$1" = "-h" ] ; then
@@ -147,11 +231,9 @@ if [ $# -ge 1 ] ; then
     fi
 fi
 
+# Old style:
+#serverSimple $PORT
 
-# bash echo
-#socat -T 1 -d -d TCP-L:10081,reuseaddr,fork,crlf SYSTEM:"echo -e \"\\\"HTTP/1.0 200 OK\\\nDocumentType: text/plain\\\n\\\ndate: \$\(date\)\\\nserver:\$SOCAT_SOCKADDR:\$SOCAT_SOCKPORT\\\nclient: \$SOCAT_PEERADDR:\$SOCAT_PEERPORT\\\n\\\"\"; cat; echo -e \"\\\"\\\n\\\"\""
-
-# bsd echo, e.g. on MAC:
-socat -T 1 -d -d TCP-L:$PORT,reuseaddr,fork,crlf SYSTEM:"echo \"\\\"HTTP/1.0 200 OK\\\nDocumentType: text/plain\\\n\\\ndate: \$\(date\)\\\nserver: \$SOCAT_SOCKADDR:\$SOCAT_SOCKPORT\\\nclient: \$SOCAT_PEERADDR:\$SOCAT_PEERPORT\\\n\\\"\"; cat; echo -e \"\\\"\\\n\\\"\""
+serverText $PORT
 
 echo "[$PROG_NAME:STATUS] Done."
---
+++ src/http-text.sh
@@ -0,0 +1,258 @@
+#!/bin/bash
+
+# #########################################
+#
+# Overview
+#
+
+
+# #########################################
+#
+# Versions
+#
+# 2020-12-21 0.01 kdk First Version
+# 2020-12-22 0.02 kdk Version with api and data
+
+PROG_NAME="http process text"
+PROG_VERSION="0.02"
+PROG_SCRIPTNAME="http-text.sh"
+
+# #########################################
+#
+# MIT license (MIT)
+#
+# Copyright 2020 Karsten Köth
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+# #########################################
+#
+# Variables
+#
+
+ServerDir="$HOME/tmp"
+logFile=""
+ProcessDir=""
+DataDir=""
+
+# Process Variables
+connectionType=""
+documentPath=""
+httpVersion=""
+
+
+# #########################################
+#
+# Functions
+#
+
+# #########################################
+# log()
+# Parameter
+#   1: Text to log
+function log()
+{
+    echo "'$PROG_SCRIPTNAME:$$:$actDateTime' '$1'" >> "$logFile"
+}
+
+# #########################################
+# sendString()
+function sendString()
+{
+    printf '%s\r\n' "$*";
+}
+
+# #########################################
+# sendHeader()
+function sendHeader()
+{
+    echo "HTTP/1.0 200 OK"
+    echo "DocumentType: text/plain"
+    echo "Access-Control-Allow-Origin: *"
+    echo ""
+}
+
+# #########################################
+# sendGreetings()
+function sendGreetings()
+{
+    echo "Welcom to my plain text."
+    echo "Nice to see you."
+    echo ""
+    echo ""
+}
+
+
+# #########################################
+# receiveHeader()
+function receiveHeader()
+{
+    # First, look for the first line from client. GET or POST or PUT
+    read -r variable 
+    # Remove newline, ...:
+    variable=${variable%%$'\r'}
+    echo "'$PROG_SCRIPTNAME:$$:$actDateTime' '$variable'" >> "$logFile"
+
+    # Typically, the first word should be "GET":
+    connectionType=$(echo "$variable" | cut -f 1 -d " " -)
+    echo "'$PROG_SCRIPTNAME:$$:$actDateTime' Connection Type: '$connectionType'" >> "$logFile"
+    documentPath=$(echo "$variable" | cut -f 2 -d " " -)
+    echo "'$PROG_SCRIPTNAME:$$:$actDateTime' Document Path:   '$documentPath'" >> "$logFile"
+    httpVersion=$(echo "$variable" | cut -f 3 -d " " -)
+    echo "'$PROG_SCRIPTNAME:$$:$actDateTime' HTTP Version:    '$httpVersion'" >> "$logFile"
+
+    # Typically, the header ends with an empty line. Therefore, we break with the first empty line:
+    while read -r header; do
+		header=${header%%$'\r'}
+		[ -z "$header" ] && break
+		echo "'$PROG_SCRIPTNAME:$$:$actDateTime' '$header'" >> "$logFile"
+	done
+}
+
+# #########################################
+# handleApi()
+function handleApi()
+{
+    # Second level in api:
+    apiCommand=$(echo "$documentPath" | cut -d "/" -f 3 -)
+    log "handleApi - secondLevel: '$apiCommand'"
+
+    # addDoubleHead
+    if   [ "$apiCommand" = "addDoubleHead" ] ; then
+        Uuid=$(uuidgen)
+        doubleHeadDir="$ProcessDir/$Uuid"
+        mkdir -p "$doubleHeadDir"
+        if [ -d "$doubleHeadDir" ] ; then
+            touch "$doubleHeadDir/value.txt"
+        fi
+        if [ -f "$doubleHeadDir/value.txt" ] ; then
+            sendString "{ \"DoubleHead\": \"$Uuid\" }"
+        else
+            sendString "Error creating file."
+        fi
+    fi
+}
+
+# #########################################
+# handleData()
+function handleData()
+{
+    # Second level in data: We check, if the uuid is known:
+    doubleHead=$(echo "$documentPath" | cut -d "/" -f 3 -)
+    log "handleData - secondLevel: '$doubleHead'"
+
+    valueFile="$ProcessDir/$doubleHead/value.txt"
+    if [ -f "$valueFile" ] ; then
+        if   [ "$connectionType" = "GET" ] ; then
+            value=$(cat $valueFile)
+            sendString "$value"
+            log "S->C: '$valueFile' : '$value' "
+        elif [ "$connectionType" = "PUT" ] ; then
+            # Not yet tested!                                       # TODO
+            read -r variable 
+            # Remove newline, ...:
+            variable=${variable%%$'\r'}
+            echo "$variable" > "$valueFile"
+            log "C->S: '$variable' -> '$valueFile'"
+        fi
+    fi
+}
+
+# #########################################
+# handleGET()
+function handleGET()
+{
+    # Look for the first level:
+    # The first field is empty - we search for "/api/"
+    firstLevel=$(echo "$documentPath" | cut -d "/" -f 2 -)
+    log "handleGet - firstLevel: '$firstLevel'"
+
+    # api - we support different commands
+    if   [ "$firstLevel" = "api" ] ; then
+        handleApi
+    # doc - we show the documentation
+    elif [ "$firstLevel" = "doc" ] ; then
+        log "handleGet - doc: Not yet supported."                   # TODO
+    # info - we show information about the settings
+    elif [ "$firstLevel" = "info" ] ; then
+        log "handleGet - info: Not yet supported."                  # TODO
+    # data - we show the data
+    elif [ "$firstLevel" = "data" ] ; then
+        handleData
+    fi
+}
+
+# #########################################
+# handlePUT()
+function handlePUT()
+{
+    # Look for the first level:
+    # The first field is empty - we search for "/api/"
+    firstLevel=$(echo "$documentPath" | cut -d "/" -f 2 -)
+    log "handlePut - firstLevel: '$firstLevel'"
+
+    if [ "$firstLevel" = "data" ] ; then
+        handleData
+    fi
+}
+
+# #########################################
+# handlePOST()
+function handlePOST()
+{
+    # Look for the first level.
+    log "handlePOST: Not yet supported."                            # TODO
+}
+
+# #########################################
+#
+# Main
+#
+
+# Start time:
+actDateTime=$(date "+%Y-%m-%d +%H:%M:%S")
+# Log file:
+mkdir -p "$ServerDir"
+logFile="$ServerDir/http-text.log"
+
+if [ $# -ge 1 ] ; then
+    log "Searching Process Directory '$1'"
+    if [ -d "$1" ] ; then
+        ProcessDir="$1"
+        log "Process Directory found."
+    else
+        log "Directory not accessible."
+    fi
+fi
+
+sendHeader
+receiveHeader
+
+if   [ "$connectionType" = "GET" ] ; then
+    handleGET
+elif [ "$connectionType" = "PUT" ] ; then
+    handlePUT
+elif [ "$connectionType" = "POST" ] ; then
+    handlePOST
+fi
+
+# sendGreetings
+
+log "Done."
