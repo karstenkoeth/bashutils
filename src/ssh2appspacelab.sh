e353e909d32af8b0905b478b57e9fbb947b1ebf6
Karsten Köth <karsten.koeth@gmail.com>
2022-09-29 19:00:52 +0200

    ~/.ssh/config used

--- src/ssh2appspacelab.sh
+++ src/ssh2appspacelab.sh
@@ -2,3 +2,14 @@
 
 ssh -i ~/.ssh/AppSpaceLab.pem  ubuntu@ec2-54-93-113-44.eu-central-1.compute.amazonaws.com
 
+# Use ~/.ssh/config instead of this file. E.g. with this content:
+#
+# Host appspacelab
+#        Hostname ec2-54-93-113-44.eu-central-1.compute.amazonaws.com
+#        User ubuntu
+#        IdentityFile ~/.ssh/AppSpaceLab.pem
+#
+#
+# With such a config file connect to the server with:
+# > ssh appspacelab
+#
