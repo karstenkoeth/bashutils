216c34274128adf4f93eeb82dadb3f72cef22bb6
Karsten Köth <karsten.koeth@gmail.com>
2024-06-14 17:57:17 +0200

    Comments changed.

--- src/install_bashutils_local.sh
+++ src/install_bashutils_local.sh
@@ -1174,6 +1174,10 @@ if [ -z "$gnuplotPresent" ] ; then
     #fi
 fi
 
+# qpdf to anaylse pdf files at command line
+# qpdf --qdf --object-streams=disable orig.pdf expanded.pdf
+# Ubuntu: install qpdf
+# MAC OS X: brew install qpdf
 
 # ################# Care about the Environment
 
