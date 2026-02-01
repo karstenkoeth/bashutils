5c261f2b73e6477e4e8a459ea47075a78ac66729
Karsten Köth <karsten.koeth@gmail.com>
2026-02-01 17:14:22 +0100

    logger included

---
+++ src/logger-executer.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+# #########################################
+#
+# Overview
+#
+# This template can be used for bash scripts.
+# See also the library with a lot of useful functions in: bashutils_common_functions.bash
+
+# #########################################
+#
+# Versions
+#
+# 2020-12-17 0.01 kdk First Version
+# 2020-12-20 0.02 kdk With actDateTime
+# 2021-01-29 0.03 kdk 2021 year ready, with PROG_DATE and Copyright in help, with showVersion()
+# 2021-02-08 0.04 kdk License text enhanced.
+
+PROG_NAME="My Script"
+PROG_VERSION="0.04"
+PROG_DATE="2021-02-08"
+PROG_CLASS="My Class of Scripts"
+PROG_SCRIPTNAME="my-script.sh"
+PROG_LIBRARYNAME="bashutils_common_functions.bash"
+
+# #########################################
+#
+# TODOs
+#
+# Give a good name and place it on git.
+
+# #########################################
+#
+# This software is licensed under
+#
+# MIT license (MIT)
+#
+# Copyright 2021 Karsten Köth
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+# #########################################
+#
+# Includes
+#
+# TODO: Given in this way, the include file must be in same directory the 
+#       script is called from. We have to auto-detect the path to the binary.
+# source bashutils_common_functions.bash
+
+
+
+# #########################################
+#
+# Constants
+#
+
+
+# #########################################
+#
+# Variables
+#
+
+# Typically, we need in a lot of scripts the start date and time of the script:
+actDateTime=$(date "+%Y-%m-%d +%H:%M:%S")
+
+# Handle output of the different verbose levels - in combination with the 
+# "echo?" functions inside "bashutils_common_functions.bash":
+ECHODEBUG="0"
+ECHOVERBOSE="0"
+ECHONORMAL="1"
+ECHOWARNING="1"
+ECHOERROR="1"
+
+# #########################################
+#
+# Functions
+#
+
+
+# #########################################
+# showHelp()
+# Parameter
+#    -
+# Return Value
+#    -
+# Show help.
+function showHelp()
+{
+    echo "[$PROG_NAME:STATUS] Program Parameter:"
+    echo "    -V     : Show Program Version"
+    echo "    -h     : Show this help"
+    echo "Copyright $PROG_DATE by Karsten Köth"
+}
+
+# #########################################
+# showVersion()
+# Parameter
+#    -
+# Return Value
+#    -
+# Show version information.
+function showVersion()
+{
+    echo "$PROG_NAME ($PROG_CLASS) $PROG_VERSION"
+}
+
+# #########################################
+#
+# Main
+#
+
+echo "[$PROG_NAME:STATUS] Starting ..."
+
+# Check for program parameters:
+if [ $# -eq 1 ] ; then
+    if [ -f "$1" ] ; then
+        echo "[$PROG_NAME:STATUS] Input file exists."
+    elif [ "$1" = "-V" ] ; then
+        showVersion ; exit;
+    elif [ "$1" = "-h" ] ; then
+        showHelp ; exit;
+    else
+        echo "[$PROG_NAME:ERROR] No input file. Exit." ; exit;
+    fi
+else
+    echo "[$PROG_NAME:ERROR] Program needs one input file as parameter. Exit."; exit;
+fi
+
+echo "[$PROG_NAME] Done."
